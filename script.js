3492536a8a6c52f7633bc9fa5dcedfbe8f2736bb
ChengJin22 <152003449+ChengJin22@users.noreply.github.com>
2024-08-14 20:51:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('data.json')
-    .then(response => response.json())
-    .then(jsonData => {
-        var calendar = d3.select('#calendar')
-            .selectAll('.day')
-            .data(jsonData)
-            .enter()
-            .append('div')
-            .attr('class', 'day')
-            .style('background-color', function(d) {
-                return d3.scaleLinear().domain([0, 500]).range(['white', 'red'])(d.TotalAmount);
-            })
-            .text(d => d.OrderDate)
-            .on('click', function(event, d) {
-                showDetails(d);
-            });
+    d3.json('data.json').then(function(data) {
+        var width = 960, height = 136, cellSize = 17;
+        var svg = d3.select("#calendar")
+            .selectAll("svg")
+            .data(d3.range(2024, 2025))
+            .enter().append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
 
-        function showDetails(d) {
-            d3.select('#details').html(`
-                <h3>${d.OrderDate}</h3>
-                <p>Total Orders: ${d.OrderID}</p>
-                <p>Total Sales: $${d.TotalAmount}</p>
-                <p>Product Types: ${d.ProductType}</p>
-            `);
-        }
+        svg.append("g")
+            .attr("text-anchor", "end")
+            .selectAll("text")
+            .data(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"])
+            .enter().append("text")
+            .attr("x", -5)
+            .attr("y", function(d, i) { return (i + 1) * cellSize; })
+            .attr("dy", "-.25em")
+            .text(function(d) { return d; });
+
+        var rect = svg.append("g")
+            .selectAll("rect")
+            .data(d3.timeDays(new Date(2024, 0, 1), new Date(2025, 0, 1)))
+            .enter().append("rect")
+            .attr("width", cellSize)
+            .attr("height", cellSize)
+            .attr("x", function(d) { return d3.timeWeek.count(d3.timeYear(d), d) * cellSize; })
+            .attr("y", function(d) { return d.getDay() * cellSize; })
+            .datum(d3.timeFormat("%Y-%m-%d"));
+
+        var color = d3.scaleQuantize()
+            .domain([0, d3.max(data, function(d) { return d.value; })])
+            .range(d3.schemeBlues[9]);
+
+        rect.filter(function(d) { return d in data; })
+            .attr("fill", function(d) { return color(data[d]); })
+            .append("title")
+            .text(function(d) { return d + ": " + data[d]; });
     });
 });
+
