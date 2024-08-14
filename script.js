b4f933934c08c0ac805951cd587126848cb20375
ChengJin22 <152003449+ChengJin22@users.noreply.github.com>
2024-08-14 21:00:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
     d3.json('data.json').then(function(data) {
-        var width = 960, height = 136, cellSize = 17;
+        var width = 960,
+            height = 136,
+            cellSize = 17;
+
         var svg = d3.select("#calendar")
             .selectAll("svg")
-            .data(d3.range(2024, 2025))
+            .data(d3.range(2020, 2022))  // Adjust year range according to your data
             .enter().append("svg")
             .attr("width", width)
             .attr("height", height)
+            .attr("class", "year")
             .append("g")
-            .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+            .attr("transform", "translate(40,20)");
 
-        svg.append("g")
-            .attr("text-anchor", "end")
-            .selectAll("text")
-            .data(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"])
-            .enter().append("text")
-            .attr("x", -5)
-            .attr("y", function(d, i) { return (i + 1) * cellSize; })
-            .attr("dy", "-.25em")
+        svg.append("text")
+            .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+            .style("text-anchor", "middle")
             .text(function(d) { return d; });
 
         var rect = svg.append("g")
+            .attr("fill", "#fff")
             .selectAll("rect")
-            .data(d3.timeDays(new Date(2024, 0, 1), new Date(2025, 0, 1)))
+            .data(function(d) { return d3.timeDays(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
             .enter().append("rect")
             .attr("width", cellSize)
             .attr("height", cellSize)
@@ -31,13 +31,22 @@ document.addEventListener("DOMContentLoaded", function() {
             .datum(d3.timeFormat("%Y-%m-%d"));
 
         var color = d3.scaleQuantize()
-            .domain([0, d3.max(data, function(d) { return d.value; })])
+            .domain([0, d3.max(data, function(d) { return d.TotalQuantity; })])
             .range(d3.schemeBlues[9]);
 
-        rect.filter(function(d) { return d in data; })
-            .attr("fill", function(d) { return color(data[d]); })
-            .append("title")
-            .text(function(d) { return d + ": " + data[d]; });
+        rect.filter(function(d) {
+            return data.find(item => item.OrderDate === d);
+        })
+        .attr("fill", function(d) {
+            var item = data.find(item => item.OrderDate === d);
+            return color(item.TotalQuantity);
+        })
+        .append("title")
+        .text(function(d) {
+            var item = data.find(item => item.OrderDate === d);
+            return d + "\nTotal Orders: " + item.TotalOrders +
+                   "\nTotal Quantity: " + item.TotalQuantity +
+                   "\nProduct Types: " + item.ProductTypes;
+        });
     });
 });
-
